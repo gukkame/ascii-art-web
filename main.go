0273c0c4e2e38af5a55a93b70295b3f73d26633a
saulitis <saulitis3@gmail.com>
2021-10-06 23:42:00 +0300

    ascii art web

--- main.go
+++ main.go
@@ -3,12 +3,57 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"strings"
 )
+
+var banner string
+
+type Asciiart struct {
+	AsciiText   []string
+	AsciiString string
+}
+
+func ReadLine(fn string, n int) string { // Returns the requested line in the font file
+	f, _ := os.Open(fn)
+	defer f.Close()
+	bf := bufio.NewReader(f)
+	var line string
+	for lineNum := 0; lineNum < n; lineNum++ {
+		line, _ = bf.ReadString('\n')
+	}
+	return line[:len(line)-1]
+}
+
+func asciiArt(text string) []string {
+	textArr := strings.Split(text, "\\n")
+	var result []string
+
+	for _, v := range text {
+		if int(v) > 126 || int(v) < 32 {
+			log.Fatal("non ascii character")
+		}
+	}
+
+	for i := 0; i < len(textArr); i++ {
+		if textArr[i] == "" { // Adds new line if multiple \n in a row
+			result = append(result, "\n")
+			continue
+		}
+
+		for z := 1; z < 9; z++ {
+			for y := 0; y < len(textArr[i]); y++ {
+				line := (int(textArr[i][y])-32)*9 + z + 1 // Finds the right place in the font file
+				result = append(result, ReadLine(banner, line))
+			}
+			result = append(result, "\n")
+		}
+	}
+	return result
+}
 
 func asciihandler(w http.ResponseWriter, r *http.Request) {
 
@@ -20,52 +65,30 @@ func asciihandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 		http.ServeFile(w, r, "tamplates/index.html")
-		return
-
 	case "POST":
 		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
 		if err := r.ParseForm(); err != nil {
 			fmt.Fprintf(w, "ParseForm() err: %v", err)
 			return
 		}
-		var asciiarr []string
-		intext := r.FormValue("text")
-		asciiarr = append(asciiarr, intext)
-		asciiarr = append(asciiarr, r.Form.Get("textstyle")+".txt")
 
-		fmt.Println(asciiarr[0])
-		fmt.Println(asciiarr[1])
-		fmt.Println(len(asciiarr))
+		inputText := r.FormValue("text")
+		banner = r.Form.Get("textstyle") + ".txt"
+		result := asciiArt(inputText)
 
-		var s []string
-		if len(asciiarr) > 1 {
-
-			incomingSentence := asciiarr[0]
-			var arr []int
-			var word string
-			//Printing words so that \n marks the start of a new line
-
-			input_slice := strings.Split(incomingSentence, "\\n")
-			for _, word := range input_slice {
-
-				var ch, position rune
-				// var i int
-
-				for _, ch = range word {
-					position = (ch - 32) * 9
-					arr = append(arr, int(position)) // array of NUMBERS of first lines of input charachters
-				}
-
-			}
-			s = Printer(word, asciiarr[1], arr)
-
-		} else {
-			fmt.Println("Please enter more than 0 arguments")
+		art := Asciiart{
+			AsciiText:   result,
+			AsciiString: strings.Join(result, ""),
 		}
 
-		AsciiString := strings.Join(s, "")
-		fmt.Fprintf(w, AsciiString)
-		//fmt.Fprintf(w, "Address = %s\n", address)
+		parsedTemplate, _ := template.ParseFiles("tamplates/asciiArt.html")
+		err := parsedTemplate.Execute(w, art)
+
+		if err != nil {
+			log.Println("Error executing template :", err)
+			return
+		}
+
 	default:
 		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
 	}
@@ -79,41 +102,4 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func Printer(input, fontName string, originalArray []int) (s []string) {
-	for lineCounter := 1; lineCounter < 9; lineCounter++ { //loop through the heigth of the ascii characters
-		for i := range originalArray {
-			oneLineofOneLetter, _ := ReadLine(fontName, originalArray[i]+lineCounter)
-			fmt.Print(oneLineofOneLetter) //just print a bunch of same "height" lines of different letters in a row
-			s = append(s, oneLineofOneLetter)
-
-		}
-		s = append(s, "\n")
-		fmt.Print(" ")
-		fmt.Print("\n") //and start a new line again
-	}
-	return s
-
-}
-
-func ReadLine(fontName string, startingLineEach int) (line string, err error) {
-
-	var lastLineOfScanner int
-
-	FontFile, err := os.Open(fontName) //open file
-	if err != nil {                    //check for errors
-		log.Fatal(err)
-	}
-	defer FontFile.Close()                //close file
-	scanner := bufio.NewScanner(FontFile) // make a scaner machine aka not doing anything yet
-	for scanner.Scan() {                  //loop through lines until you reached the wished line aka start machine
-
-		if lastLineOfScanner == startingLineEach {
-			return scanner.Text(), scanner.Err() //scanner.Text() is where you store the result of scanner you can print it aswell if you make it equal to a value
-
-		}
-		lastLineOfScanner++
-	}
-	return line, io.EOF // it will return the line when the scanner mathces reading to the input line number aka gets to it
 }
